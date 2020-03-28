8b884eaf25944dbe812d53bc5301f78274f42cdd
feridun Akyol <feridunakyol34@gmail.com>
2020-03-28 15:00:18 +0100

    refactoring

---
+++ listeners/listener.js
@@ -0,0 +1,6 @@
+document.getElementById('addTodo').addEventListener('click', addTodo);
+document.getElementById('changeTodo').addEventListener('click', changeTodo);
+document.getElementById('toggleCompleted').addEventListener('click', toggleCompleted);
+document.getElementById('toggleAll').addEventListener('click', toggleAll);
+
+document.querySelector('ul').addEventListener('click', deleteTodo);
